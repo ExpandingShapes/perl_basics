e55788812b711e4e070aa7f907c0d89f9acd5680
Alexander <lapseofreason17@gmail.com>
2020-10-27 13:06:24 +0300

    Create 01.pl

---
+++ src/01.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+$, = " ";
+
+# Given an array of integers, return the array in ascending order
+# with duplicate elements removed. Separate array's elements by whitespace.
+
+sub get_unique_sorted{
+    my @arr = @{shift @_};
+    my %seen = ();
+    return sort grep { !$seen{$_}++ } @arr;
+}
+
+#testing
+my $test_arr_ref = [0, 6, 2, 2, 3, 1, 1, 74];
+print get_unique_sorted($test_arr_ref);
